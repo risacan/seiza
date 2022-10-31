86081a10f3b9aa565bd9b6d72314953c270632f6
Risa Watanabe Harada <supikateriburu@gmail.com>
2022-10-31 19:32:10 +0900

    🔧 Add Spec

--- lib/seiza.rb
+++ lib/seiza.rb
@@ -4,5 +4,25 @@ require_relative "seiza/version"
 
 module Seiza
   class Error < StandardError; end
-  # Your code goes here...
+  class << Time
+
+    def seiza
+      super.month
+    end
+
+    SEIZA_NAMES = %w(
+      aries
+      taurus
+      gemini
+      cancer
+      leo
+      virgo
+      libra
+      scorpio
+      sagittarius
+      capricorn
+      aquarius
+      pisces
+    )
+  end
 end
