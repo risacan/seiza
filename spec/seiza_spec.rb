5e2e13efc6fde1e65b90aecc56bf81173fe9fd04
Risa Watanabe Harada <supikateriburu@gmail.com>
2022-12-23 10:28:02 +0900

    ✨ Add seiza module

--- spec/seiza_spec.rb
+++ spec/seiza_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe Seiza do
   it "return seiza on July 2nd" do
     expect(Time.new(1991, 07, 02).seiza).to eq("cancer")
   end
+
+  it "return seiza on June 3rd" do
+    expect(Time.new(1989, 06, 03).zodiac_sign).to eq("gemini")
+  end
 end
